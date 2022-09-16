f93037f0d8519302c983c6808186739c1d93f47b
Maksimchik36 <Peltek1985@gmail.com>
2022-09-16 17:03:56 +0300

    3 step of task completed

--- contacts.js
+++ contacts.js
@@ -2,39 +2,79 @@
 const fs = require("fs/promises");    // 1-ый вариант
 // const fs = require("fs").promises; // 2-ой вариант
 const path = require("path");
+const {nanoid} = require("nanoid")
 
 // метод resolve позволяет нам получить абсолютный путь из относительного пути 
 const contactsPath = path.resolve('./db/contacts.json');
-// console.log(contactsPath);
+
+// записывает allContacts в contacts.json (создана в результате рефакторинга, используется при изменении массива для обновления и на бэкэнде)
+const updateContacts = async (allContacts) => await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
 
 
-function listContacts() {
-    fs.readFile(contactsPath)
-        .then(data => console.log(JSON.parse(data)))
-        .catch(error => console.log(error))
+// получает список всех контактов
+async function getListContacts() {
+    const data = await fs.readFile(contactsPath);
+    const result = JSON.parse(data);
+    console.log(result);
+    return result;
 }
 
 
-function getContactById(contactId) {
-    // const data = fs.readFile(contactsPath);
-    // return data;
+// получает контакт по id
+async function getContactById(contactId) {
+    const id = contactId.toString();
+    const allContacts = await getListContacts();
+    const contactById = allContacts.find(contact => contact.id === id);
+    console.log(contactById);
+    return contactById || null;
 }
 
 
-function removeContact(contactId) {
-    // const data = fs.writeFile(contactsPath);
-    // return data;
+// // удаляет контакт по id - 1-й вариант
+// async function removeContact(contactId) {
+//     const id = contactId.toString();
+//     const allContacts = await getListContacts();
+//     const unremovedContacts = allContacts.filter(contact => contact.id !== id);
+//     await fs.writeFile(contactsPath, JSON.stringify(unremovedContacts, null, 2));
+//     const deletedContact = allContacts.find(contact => contact.id === id);
+//     return deletedContact;
+// }
+
+
+// удаляет контакт по id - 2-й вариант
+async function removeContact(contactId) {
+    const id = contactId.toString();
+    const allContacts = await getListContacts();
+    // вычисляет index указанного элемента в массиве
+    const index = allContacts.findIndex(item => item.id === id);
+    if(index === -1){
+        return null;
+    }
+    // удаляет элемент из массива и возвращает удаляемый элемент (splice)
+    const result = allContacts.splice(index, 1);
+    await updateContacts(allContacts);
+    return result;
 }
 
 
-function addContact(name, email, phone) {
-    // const data = fs.writeFile(contactsPath);
-    // return data;
+// добавляет новый контакт
+async function addContact(name, email, phone) {
+    const allContacts = await getListContacts();
+    const id = nanoid();
+    const newContact = {
+        id,
+        name,
+        email,
+        phone
+    }
+    allContacts.push(newContact);
+    await updateContacts(allContacts);
+    return newContact;
 }
 
 
 module.exports = {
-    listContacts,
+    getListContacts,
     getContactById,
     removeContact,
     addContact,
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const listContacts = require('./contacts').listContacts;
+const getListContacts = require('./contacts').getListContacts;
 const getContactById = require('./contacts').getContactById;
 const removeContact = require('./contacts').removeContact;
 const addContact = require('./contacts').addContact;
 
-console.log(listContacts());
+
+// getListContacts();
+// getContactById(7);
+// removeContact(4);
+// addContact("Hulk", "Green22@gmail.com", "(348) 541-2836" );
